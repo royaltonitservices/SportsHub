546c4759ed6b8e926535ee8155bff1f857c80a3b
RoyaltonITServices <royaltonitservices@gmail.com>
2026-02-17 07:55:01 -0800

    Initial Commit

---
+++ SportsHub/SportsHubApp.swift
@@ -0,0 +1,32 @@
+//
+//  SportsHubApp.swift
+//  SportsHub
+//
+//  Created by Aarush Khanna  on 2/17/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SportsHubApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SportsHubTests/SportsHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportsHubTests.swift
+//  SportsHubTests
+//
+//  Created by Aarush Khanna  on 2/17/26.
+//
+
+import Testing
+@testable import SportsHub
+
+struct SportsHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
